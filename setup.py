9e276258c253107a3ebc373364e4f2ec496be0d8
Tomáš Mlynář <tom.mlynar1@gmail.com>
2022-08-01 23:49:15 +0200

    version number changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='witokit_new',
-   version='0.1.1',
+   version='0.1.2',
    description='Re-implementation of part of witokit package',
    author='Tomas Mlynar',
    author_email='tomas@mlynar.ml',
