aef2123e774680fcca9fc7b3490cbcbfb97cd678
Tomáš Mlynář <tom.mlynar1@gmail.com>
2022-08-01 23:15:31 +0200

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='witokit_new',
+   version='0.1',
+   description='Re-implementation of part of witokit package',
+   author='Tomas Mlynar',
+   author_email='tomas@mlynar.ml',
+   packages=['witokit_new'],  #same as name
+   install_requires=['tqdm', 'bs4', 'bz2'], #external packages as dependencies
+)
